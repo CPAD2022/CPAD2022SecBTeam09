dc2e29855141eb564b7367e003b8581d60510973
rajesh-ndv <rajubhai182000@gmail.com>
2022-12-02 11:22:30 +0530

    App setup

---
+++ newsfeed/lib/src/App.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_flavor/flutter_flavor.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+
+class App extends StatelessWidget {
+  final AuthenticationRepository authenticationRepository =
+  AuthenticationRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+
+}

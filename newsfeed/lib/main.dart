b2f61dda52955b488e097e453122018ea7c583fd
rajesh-ndv <rajubhai182000@gmail.com>
2022-12-02 22:59:53 +0530

    Import Statement Error Fix

--- newsfeed/lib/main.dart
+++ newsfeed/lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:flutter/material.dart';
-
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
+import 'package:news/src/App.dart';
+import 'package:news/src/blocs/language_bloc/language_bloc.dart';
+import 'package:news/src/models/article/article_model.dart';
+import 'package:news/src/models/category/category.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:news/src/models/user/user.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:flutter_flavor/flutter_flavor.dart';
 

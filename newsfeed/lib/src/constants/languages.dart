5ea966507d13a4a05ae324d77d64cea33cade45d
jahnavisaddala <2021sp93071@wilp.bits-pilani.ac.in>
2022-12-02 17:15:10 +0530

    added languages

---
+++ newsfeed/lib/src/constants/languages.dart
@@ -0,0 +1,15 @@
+import 'package:news/src/constants/enums.dart';
+
+var supportedLanguages = {
+  'English': Language.EN,
+  'Serbian': Language.SR,
+  'German': Language.DE,
+  'Spanish': Language.ES,
+  'French': Language.FR,
+  'Italian': Language.IT,
+  'Dutch': Language.NL,
+  'Norwegian': Language.NO,
+  'Portuguese': Language.PT,
+  'Russian': Language.RU,
+  'Chinese': Language.ZH,
+};

d3b5bc6b2115cc67fc863a44b05d288e7fa0da55
rajesh-ndv <rajubhai182000@gmail.com>
2022-12-02 14:51:09 +0530

    Added white and dark theme

---
+++ newsfeed/lib/src/utils/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData kLightTheme = _buildLightTheme();
+
+final TextTheme textThemeLight =
+    GoogleFonts.latoTextTheme(ThemeData.light().textTheme).copyWith(
+  headline4: TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 28,
+    color: Colors.black,
+  ),
+  subtitle2: TextStyle(
+    color: HexColor.fromHex(ColorConstants.primaryColor),
+  ),
+);
+
+final TextTheme textThemeDark =
+    GoogleFonts.latoTextTheme(ThemeData.dark().textTheme).copyWith(
+  headline4: TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 28,
+    color: Colors.white,
+  ),
+  subtitle2: TextStyle(
+    color: HexColor.fromHex(ColorConstants.primaryColor),
+  ),
+);
+
+ThemeData _buildLightTheme() {
+  return ThemeData(brightness: Brightness.light, textTheme: textThemeLight);
+}
+
+final ThemeData kDarkTheme = _buildDarkTheme();
+
+ThemeData _buildDarkTheme() {
+  return ThemeData(brightness: Brightness.dark, textTheme: textThemeDark);
+}

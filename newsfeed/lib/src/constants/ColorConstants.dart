fe3f56d44615cf26bc7511cf9d9230ac50d74aad
jahnavisaddala <2021sp93071@wilp.bits-pilani.ac.in>
2022-12-02 16:07:11 +0530

    Added ColorConstants

---
+++ newsfeed/lib/src/constants/ColorConstants.dart
@@ -0,0 +1,25 @@
+class ColorConstants {
+  static String primaryColor = "#1942f5";
+  static String secondaryColor = "#EFEFEF";
+  static String silverGray = "#C0C0C0";
+  static String primaryTextColor = "#EFEFEF";
+  static String selectedTextColor = "#EFEFEF";
+  static String callToAction = "#1942f5";
+  static String callToActionDisables = "#b8c5fc";
+
+  static void setLightColors() {
+    primaryColor = "#1942f5";
+    secondaryColor = "#EFEFEF";
+    primaryTextColor = "303030";
+    callToAction = "#1942f5";
+    callToActionDisables = "#b8c5fc";
+  }
+
+  static void setDarkColors() {
+    primaryColor = "#191919";
+    secondaryColor = "#262626";
+    primaryTextColor = "#EFEFEF";
+    callToAction = "#3DDCBA";
+    callToActionDisables = "#BAFCEE";
+  }
+}
